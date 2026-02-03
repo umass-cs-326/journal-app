4b75e1af4eee9d4a18c4a27259b1153f6a8e9a03
Tim Richards <richards@cs.umass.edu>
2026-02-02 19:01:21 -0500

    step-02

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { IApp } from "./contracts";
 import express from "express";
+// We need these types to type our route handlers
+import { Request, Response } from "express";
 
 /**
  * ExpressApp implements IApp.
@@ -12,6 +14,27 @@ export class ExpressApp implements IApp {
 
   constructor() {
     this.app = express();
+    // Register middleware and routes
+    this.registerMiddleware();
+    this.registerRoutes();
+  }
+
+  registerMiddleware(): void {
+    // Apply any global middleware here, e.g., logging, CORS, etc.
+    // Need this middleware to serve static files (CSS, JS, images, etc.)
+    this.app.use(express.static("static"));
+  }
+
+  registerRoutes(): void {
+    // Home route serving a simple HTML page
+    this.app.get("/", (_req: Request, res: Response) => {
+      // In our last example, we sent a string of HTML.
+      // Now, we serve a static HTML file from the "static" directory.
+      // We could have relied on express.static middleware to do this,
+      // but this shows how to send a specific file for a route. The difference
+      // is in the URL: "/" vs "/journal.html".
+      res.sendFile("journal.html", { root: "static" });
+    });
   }
 
   getExpressApp(): express.Express {
